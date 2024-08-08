a80b0e9302c51e6387e3d08a1098f3b20217d596
MD Waliullah <waliullah9099@gmail.com>
2024-08-08 17:21:23 +0600

    working in services section

--- src/components/UI/Home/Services/Services.jsx
+++ src/components/UI/Home/Services/Services.jsx
@@ -1,6 +1,56 @@
 import SectionTitle from "../../SectionTitle/SectionTitle";
 
 const Services = () => {
+  const services = [
+    {
+      id: 1,
+      ttile: "Brand Strategy and Architecture",
+      description:
+        "We are your go-to place if you want a compelling brand identity. Whether it's crafting We are your go-to place if you want a compelling brand identity.",
+      image: "/public/assets/images/services.png",
+      variant: "B389C9",
+    },
+    {
+      id: 2,
+      ttile: "Advertising Content Development",
+      description:
+        "They say content is king. Well our content is not just catchy but downright magnet. They say content is king. Well our content is not just catchy but downright magnetic!",
+      image: "/public/assets/images/services.png",
+      variant: "62C288",
+    },
+    {
+      id: 3,
+      ttile: "Web & Search Experience",
+      description:
+        "Conquering the web for you is something we specialize in. With our Web, UX/UI, and Conquering the web for you is something we specialize in. With our Web, UX/UI, and...",
+      image: "/public/assets/images/services.png",
+      variant: "E37083",
+    },
+    {
+      id: 4,
+      ttile: "Brand Strategy and Architecture",
+      description:
+        "We are your go-to place if you want a compelling brand identity. Whether it's crafting We are your go-to place if you want a compelling brand identity.",
+      image: "/public/assets/images/services.png",
+      variant: "E9B449",
+    },
+    {
+      id: 5,
+      ttile: "Advertising Content Development",
+      description:
+        "They say content is king. Well our content is not just catchy but downright magnet. They say content is king. Well our content is not just catchy but downright magnetic!",
+      image: "/public/assets/images/services.png",
+      variant: "76C3DC",
+    },
+    {
+      id: 6,
+      ttile: "Web & Search Experience",
+      description:
+        "Conquering the web for you is something we specialize in. With our Web, UX/UI, and Conquering the web for you is something we specialize in. With our Web, UX/UI, and...",
+      image: "/public/assets/images/services.png",
+      variant: "31BAAE",
+    },
+  ];
   return (
     <>
       <div className="mt-32"></div>
@@ -9,6 +59,7 @@ const Services = () => {
         title="Our Services"
         description="We take digital experience to the next level"
       />
+      <div className="grid grid-cols-1 md:col-end-3 lg:grid-cols-4"></div>
     </>
   );
 };

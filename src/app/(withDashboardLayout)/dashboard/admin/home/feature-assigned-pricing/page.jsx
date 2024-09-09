380031c907e151a9ced118277f110732599f4846
Md. Omar Faruk <mdomarfaruk149518@gmail.com>
2024-09-09 10:21:53 +0600

    [initialized]assigned feature to pricing

---
+++ src/app/(withDashboardLayout)/dashboard/admin/home/feature-assigned-pricing/page.jsx
@@ -0,0 +1,12 @@
+import FeatureAssignedToPricing from "@/components/Dashboard/FeatureAssignedToPricing/FeatureAssignedToPricing";
+import React from "react";
+
+const FeatureAssignedToPricingPage = () => {
+  return (
+    <>
+      <FeatureAssignedToPricing />
+    </>
+  );
+};
+
+export default FeatureAssignedToPricingPage;

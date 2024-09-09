4a34b0dcbe1bc64d9fd2026557b5526deb50bff4
Md. Omar Faruk <mdomarfaruk149518@gmail.com>
2024-09-09 08:34:38 +0600

    [Done]pricingCategoryApi

---
+++ src/components/Dashboard/PricingCategory/PricingCategory.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const PricingCategory = () => {
+  return <div></div>;
+};
+
+export default PricingCategory;
---
+++ src/redux/api/pricingCategory.js
@@ -0,0 +1,60 @@
+import { baseApi } from "./baseApi";
+
+const pricingCategoryApi = baseApi.injectEndpoints({
+  endpoints: (builder) => ({
+    // Mutation for creating a new Pricing Category
+    createPricingCategory: builder.mutation({
+      query: (data) => ({
+        url: "/pricing-categories",
+        method: "POST",
+        data,
+      }),
+      invalidatesTags: ["pricing-category"],
+    }),
+
+    // Query for fetching all Pricing Category
+    getAllPricingCategories: builder.query({
+      query: () => ({
+        url: "/pricing-categories",
+        method: "GET",
+      }),
+      providesTags: ["pricing-category"],
+    }),
+
+    // Query for fetching a single Pricing Category by its ID
+    getSingleOurService: builder.query({
+      query: (id) => ({
+        url: `/pricing-categories/${id}`,
+        method: "GET",
+      }),
+      providesTags: ["pricing-category"],
+    }),
+
+    // Mutation for updating an existing Pricing Category
+    updatePricingCategory: builder.mutation({
+      query: ({ id, data }) => ({
+        url: `/pricing-categories/${id}`,
+        method: "PATCH",
+        data,
+      }),
+      invalidatesTags: ["pricing-category"],
+    }),
+
+    // Mutation for deleting a Pricing Category by its ID
+    deleteOurService: builder.mutation({
+      query: (id) => ({
+        url: `/pricing-categories/${id}`,
+        method: "DELETE",
+      }),
+      invalidatesTags: ["pricing-category"],
+    }),
+  }),
+});
+
+export const {
+  useCreatePricingCategoryMutation,
+  useGetAllPricingCategoriesQuery,
+  useGetSingleOurServiceQuery,
+  useUpdatePricingCategoryMutation,
+  useDeleteOurServiceMutation,
+} = pricingCategoryApi;

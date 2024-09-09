b8df91b3b73cf2fbc2e643112247f9f71cd63837
Md. Omar Faruk <mdomarfaruk149518@gmail.com>
2024-09-09 08:51:52 +0600

    [Done]pricing

---
+++ src/redux/api/pricingApi.js
@@ -0,0 +1,60 @@
+import { baseApi } from "./baseApi";
+
+const pricingApi = baseApi.injectEndpoints({
+  endpoints: (builder) => ({
+    // Mutation for creating a new Pricing
+    createPricing: builder.mutation({
+      query: (data) => ({
+        url: "/pricing",
+        method: "POST",
+        data,
+      }),
+      invalidatesTags: ["pricing"],
+    }),
+
+    // Query for fetching all Pricing
+    getAllPricing: builder.query({
+      query: () => ({
+        url: "/pricing",
+        method: "GET",
+      }),
+      providesTags: ["pricing"],
+    }),
+
+    // Query for fetching a single Pricing by its ID
+    getSingleOurService: builder.query({
+      query: (id) => ({
+        url: `/pricing/${id}`,
+        method: "GET",
+      }),
+      providesTags: ["pricing"],
+    }),
+
+    // Mutation for updating an existing Pricing
+    updatePricing: builder.mutation({
+      query: ({ id, data }) => ({
+        url: `/pricing/${id}`,
+        method: "PATCH",
+        data,
+      }),
+      invalidatesTags: ["pricing"],
+    }),
+
+    // Mutation for deleting a Pricing by its ID
+    deleteOurService: builder.mutation({
+      query: (id) => ({
+        url: `/pricing/${id}`,
+        method: "DELETE",
+      }),
+      invalidatesTags: ["pricing"],
+    }),
+  }),
+});
+
+export const {
+  useCreatePricingMutation,
+  useGetAllPricingQuery,
+  useGetSingleOurServiceQuery,
+  useUpdatePricingMutation,
+  useDeleteOurServiceMutation,
+} = pricingApi;

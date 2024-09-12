f705981d5da762affc2fffcd60e0e269c8196dfd
alive1258 <javashazib@gmail.com>
2024-09-12 11:12:19 +0600

    color daymanica dd

--- src/components/UI/Home/HomeService/HomeServiceCard.jsx
+++ src/components/UI/Home/HomeService/HomeServiceCard.jsx
@@ -1,7 +1,4 @@
-"use client";
-
 import Link from "next/link";
-
 import CardSection from "./CardSection";
 
 const HomeServiceCard = ({ service }) => {
@@ -9,12 +6,9 @@ const HomeServiceCard = ({ service }) => {
     <>
       <Link href={`/services#${service?.slug || service?.id}`}>
         <div
-          className={`text-center md:h-[300px] reveal cursor-pointer px-2 py-4 hover:shadow-lg hover:scale-105 transition-all duration-300 ease-in-out rounded-lg bg-white`}
-          onMouseEnter={(e) => {
-            e.currentTarget.style.boxShadow = `0px 0px 16px 3px ${service?.color_code}`; // Apply dynamic shadow on hover
-          }}
-          onMouseLeave={(e) => {
-            e.currentTarget.style.boxShadow = `0 4px 6px rgba(0, 0, 0, 0.1)`; // Revert to default shadow
+          className={`text-center md:h-[300px] reveal cursor-pointer px-2 py-4 transition-all duration-300 ease-in-out rounded-lg bg-white dynamic-hover`}
+          style={{
+            "--dynamic-shadow": `0px 0px 16px 0px ${service?.color_code}`,
           }}
         >
           <CardSection service={service} />
